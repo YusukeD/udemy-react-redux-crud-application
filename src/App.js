7503b7a1c49ce38a9a7a557ab9d49931f045e9f7
Yusuke D <daifuku@metaps-payment.com>
2020-04-06 10:55:00 +0900

    create class components and functional component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
-function App() {
-  return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => console.log("Hi!")} />
-      </React.Fragment>
-  );
-}
+// function App() {
+//   return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => console.log("Hi!")} />
+//       </React.Fragment>
+//   );
+// }
+
+const App = () => {
+    return <div>
+        <Cat/>
+        <Cat/>
+        <Cat/>
+        <Cat/>
+    </div>
+};
+
+const Cat = () => <div>Meow</div>;
 
 export default App;
