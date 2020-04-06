97d0c2dd135e482872fd7a19e75045c6375a27e2
Yusuke D <daifuku@metaps-payment.com>
2020-04-06 10:40:03 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <div><h1>Hello, world!</h1></div>;
+  return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => console.log("Hi!")} />
+      </React.Fragment>
+  );
 }
 
 export default App;
