c0a1ed3bf1e614601d4abf517cd439fe55b74156
Yusuke D <daifuku@metaps-payment.com>
2020-04-20 11:52:45 +0900

    apply material-ui to eventsIndex

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from "redux-thunk";
-import { BrowserRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import {composeWithDevTools} from 'redux-devtools-extension';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import reducer from './reducers'
 import './index.css';
@@ -20,16 +21,18 @@ const store = createStore(reducer, enhancer);
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <Switch>
-                    <Route path="/events/new" component={EventsNew}/>
-                    <Route path="/events/:id" component={EventsShow}/>
-                    <Route exact path="/" component={EventsIndex}/>
-                    <Route exact path="/events" component={EventsIndex}/>
-                </Switch>
-            </BrowserRouter>
-        </Provider>
+        <MuiThemeProvider>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/events/new" component={EventsNew}/>
+                        <Route path="/events/:id" component={EventsShow}/>
+                        <Route exact path="/" component={EventsIndex}/>
+                        <Route exact path="/events" component={EventsIndex}/>
+                    </Switch>
+                </BrowserRouter>
+            </Provider>
+        </MuiThemeProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
